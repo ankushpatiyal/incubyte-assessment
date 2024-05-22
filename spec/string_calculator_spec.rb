bf1cddc7abd56848279b6c5e84d82f9fa3fa82f3
ankushpatiyal <ankushpatiyal@gmail.com>
2024-05-22 07:56:56 +0530

    add method returns the same number if only one number is given in string

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,11 +1,18 @@
 require_relative '../string_calculator'
 
 describe StringCalculator do
-  let(:calculator) { StringCalculator.new }
+  let!(:calculator) { StringCalculator.new }
+
   describe '#add' do
     context "when empty string is given" do
       it 'returns 0' do
         expect(calculator.add("")).to eq(0)
+      end
+    end
+
+    context 'when only one number is given' do
+      it 'returns that number' do
+        expect(calculator.add("3")).to eq(3)
       end
     end
   end
