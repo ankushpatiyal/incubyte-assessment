edcdafd9c90ab00203c0edbc3d5625290277157d
ankushpatiyal <ankushpatiyal@gmail.com>
2024-05-22 11:45:51 +0530

    refactoring code

--- string_calculator.rb
+++ string_calculator.rb
@@ -5,16 +5,12 @@ class StringCalculator
 
     raise Errors::InvalidInputError if numbers[-1].match?(/[^\d]\z/)
 
+    split_operator = %r{[\n,]}
+
     if numbers.match?(/\A\/\/(.)\n/)
       split_operator = %r{[#{numbers.match(/\A\/\/(.)\n/)[1]}]}
-      negative_numbers = []
-      numbers.split(split_operator).map{|val| negative_numbers << val.to_i if val.to_i < 0}
-      raise Errors::NegativeInputError.new(negative_numbers) if negative_numbers.length > 0
-
-      return numbers.split(split_operator).inject(0) {|total, val| total + val.to_i}
     end
 
-    split_operator = %r{[\n,]}
     negative_numbers = []
     numbers.split(split_operator).map{|val| negative_numbers << val.to_i if val.to_i < 0}
     raise Errors::NegativeInputError.new(negative_numbers) if negative_numbers.length > 0
