fd58edfb4ff7714f031a8757630e97a8135fcb54
ankushpatiyal <ankushpatiyal@gmail.com>
2024-05-22 08:17:29 +0530

    treat newline as a separator

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -21,5 +21,17 @@ describe StringCalculator do
         expect(calculator.add("3,5")).to eq(8)
       end
     end
+
+    context 'when newline is separator between numbers' do
+      it 'gives sum of the numbers present' do
+        expect(calculator.add("3\n5")).to eq(8)
+      end
+    end
+
+    context 'when newline and comma both are treated as separators' do
+      it 'tgives sum of the numbers present' do
+        expect(calculator.add("3\n5,6")).to eq(14)
+      end
+    end
   end
 end
--- string_calculator.rb
+++ string_calculator.rb
@@ -2,6 +2,6 @@ class StringCalculator
   def add(numbers)
     return 0 if numbers.empty?
 
-    numbers.split(",").inject(0) { |total, val| total + val.to_i }
+    numbers.split(/[\n,]/).inject(0) { |total, val| total + val.to_i }
   end
 end
